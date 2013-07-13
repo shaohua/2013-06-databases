37cdbe815c59e96e5c17cb02d5e19cdd74b096aa
Hack Reactor Students <github@hackreactor.com>
2013-07-12 19:38:31 -0700

    3 tables now

--- SQL/schema.sql
+++ SQL/schema.sql
@@ -1,17 +1,31 @@
--- DROP DATABASE chat;
+DROP DATABASE chat;
 
 CREATE DATABASE chat;
 
 USE chat;
+
+CREATE TABLE users (
+  u_id int NOT NULL AUTO_INCREMENT,
+  username varchar(255),
+  PRIMARY KEY (u_id)
+);
 
 CREATE TABLE messages (
  /* Describe your table here.*/
  msg_id int NOT NULL AUTO_INCREMENT,
  room varchar(30),
  createdAt timestamp,
- username varchar(255),
+ u_id int,
  text text,
- PRIMARY KEY (msg_id)
+ PRIMARY KEY (msg_id),
+ FOREIGN KEY (u_id) REFERENCES users(u_id)
+);
+
+CREATE TABLE friends (
+  u1_id int,
+  u2_id int,
+  FOREIGN KEY (u1_id) REFERENCES users(u_id),
+  FOREIGN KEY (u2_id) REFERENCES users(u_id)
 );
 
 /* You can also create more tables, if you need them... */
