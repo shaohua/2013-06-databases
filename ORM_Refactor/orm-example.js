48a6a1d7a95a7c408a14a93a01051e7931da4f12
Hack Reactor Students <github@hackreactor.com>
2013-07-13 11:41:47 -0700

    Add username and pass for mysql

--- ORM_Refactor/orm-example.js
+++ ORM_Refactor/orm-example.js
@@ -4,8 +4,8 @@
  */
 
 var Sequelize = require("sequelize");
-var sequelize = new Sequelize("chat", "bacon", "pass");
-/* TODO this constructor takes the database name, username, then password.
+var sequelize = new Sequelize("chat", "root", "");
+/* this constructor takes the database name, username, then password.
  * Modify the arguments if you need to */
 
 /* first define the data structure by giving property names and datatypes
