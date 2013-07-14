afc915df77316b6ec340a8e3cac7720ae295d0ff
Hack Reactor Students <github@hackreactor.com>
2013-07-13 21:10:26 -0700

    Works with ORM

--- ORM_Refactor/persistent_server.js
+++ ORM_Refactor/persistent_server.js
@@ -15,49 +15,25 @@ var Message = sequelize.define('Message', {
   text: Sequelize.TEXT
 });
 
-Message.belongsTo(User);
 User.hasMany(Message);
+Message.belongsTo(User);
 
 // Create the tables:
 Message.sync(); // will emit success or failure event
-User.sync().success(function() {
+User.sync();
 
-
-
-  User.findAll({ where: {username: "Jean Valjean"} }).success(function(usrs) {
-    // This function is called back with an array of matches.
-    for (var i = 0; i < usrs.length; i++) {
-      console.log(usrs[i].username + " exists");
-    }
+exports.get_msg = function(room, callback){
+  Message.findAll({ include: [User], where: { room: room } }).success(function(messages){
+    var result = _.map(messages, function(message){
+      message.username = message.user.username;
+      delete message['user'];
+      return message.values;
+    });
+    callback(result);
   });
+};
 
-  User
-    .findOrCreate({username:'ellllllllllllllllll'})
-    .success(function(user, created){
-      console.log('user: ', user, '\n');
-      console.log(user.values, created);
-    });
-
-  Message
-    .create({room:'messages', text:'msg1', username:'ellllllllllllllllll'})
-    .success(function(){
-
-    });
-
-});
-
-
-
-
-// exports.get_msg = function(room, callback){
-//   dbConnection.query('SELECT messages.room, messages.createdAt, messages.text, users.username from messages INNER JOIN users ON messages.u_id = users.u_id WHERE room=?', room, function(err, rows) {
-//     if (err) throw err;
-//     // console.log('The rows are: ', rows);
-//     callback(rows);
-//   });
-// };
-
-
+//exports.get_msg('messages');
 
 // var _get_u_id = function(username, callback){
 //   dbConnection.query('SELECT u_id FROM users WHERE username=?',
@@ -88,20 +64,15 @@ User.sync().success(function() {
 //   });
 // };
 
-// exports.post_msg = function(msgData, callback){
-//   _get_u_id(msgData.username, function(result){
-//     if(result.length){
-//       _(msgData).extend({u_id: result[0].u_id});
-//       delete msgData['username'];
-//       _post_anyone(msgData);
-//     } else {
-//       _insert_new_user(msgData.username, function(result){
-//         delete msgData['username'];
-//         _(msgData).extend({u_id: result.insertId});
-//         _post_anyone(msgData);
-//       });
-//     }
-//   });
-
-//   callback();
-// };
+exports.post_msg = function(msgData, callback){
+  User
+    .findOrCreate({username:msgData.username})
+    .success(function(user, created){
+      Message
+        .create({room:msgData.room, text:msgData.text, UserId:user.values.id})
+        .success(function(message2, created){
+          console.log("message created: ", message2);
+        });
+    });
+  callback();
+};
