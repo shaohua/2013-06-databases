efafca52efd6484c72f1a3a6f5d74022ae542adb
Hack Reactor Students <github@hackreactor.com>
2013-07-13 21:20:46 -0700

    ORM semi final commit

--- ORM_Refactor/persistent_server.js
+++ ORM_Refactor/persistent_server.js
@@ -2,6 +2,7 @@ var mysql = require('mysql');
 var _ = require('underscore');
 var Sequelize = require("sequelize");
 
+//we are using chat2!!!
 var sequelize = new Sequelize("chat2", "root", "");
 
 // setting up objects and relationships
@@ -18,51 +19,24 @@ var Message = sequelize.define('Message', {
 User.hasMany(Message);
 Message.belongsTo(User);
 
-// Create the tables:
-Message.sync(); // will emit success or failure event
+// Create the tables if necessary:
+Message.sync();
 User.sync();
 
 exports.get_msg = function(room, callback){
-  Message.findAll({ include: [User], where: { room: room } }).success(function(messages){
-    var result = _.map(messages, function(message){
-      message.username = message.user.username;
-      delete message['user'];
-      return message.values;
+  Message
+    .findAll({ include: [User], where: { room: room } })
+    .success(function(messages){
+      var result = _.map(messages, function(message){
+        //get username from the message.user obj
+        //then delete the message.user obj
+        message.username = message.user.username;
+        delete message['user'];
+        return message.values;
+      });
+      callback(result);
     });
-    callback(result);
-  });
 };
-
-//exports.get_msg('messages');
-
-// var _get_u_id = function(username, callback){
-//   dbConnection.query('SELECT u_id FROM users WHERE username=?',
-//     username, function(err, result) {
-//     if (err) {
-//       console.log(err);
-//     } else {
-//       callback(result);
-//     }
-//   });
-// };
-
-// var _insert_new_user = function(username, callback){
-//   dbConnection.query('INSERT INTO users SET ?', {username: username}, function(err, result) {
-//     if(err) {
-//       console.log(err);
-//     } else {
-//       callback(result);
-//     }
-//   });
-// };
-
-// var _post_anyone = function(msgData){
-//   dbConnection.query('INSERT INTO messages SET ?', msgData, function(err, result) {
-//     if(err) {
-//       console.log(err);
-//     }
-//   });
-// };
 
 exports.post_msg = function(msgData, callback){
   User
@@ -71,8 +45,7 @@ exports.post_msg = function(msgData, callback){
       Message
         .create({room:msgData.room, text:msgData.text, UserId:user.values.id})
         .success(function(message2, created){
-          console.log("message created: ", message2);
+          callback();
         });
     });
-  callback();
 };
